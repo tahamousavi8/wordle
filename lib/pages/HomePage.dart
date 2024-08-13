cbc9ced58dcff4036d5bb498256efff154eceaa7
Taha Mousavi <tahamousavi6@gmail.com>
2024-08-13 16:20:17 +0330

    icons added

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wordle/components/my_icon.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,22 +7,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [
-                            Color.fromRGBO(64, 75, 93, 1),
-                            Color.fromRGBO(84, 121, 137, 1),
-                          ]),
+        body: Stack(
+      children: [
+        Positioned.fill(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color.fromRGBO(64, 75, 93, 1),
+                    Color.fromRGBO(84, 121, 137, 1),
+                  ]),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MyIcon(
+                    myIcon: Icons.help_outline_outlined,
+                    myColor: Colors.lightBlue,
+                  ),
+                  MyIcon(
+                    myIcon: Icons.settings,
+                    myColor: Color.fromRGBO(218, 147, 188, 1),
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
               ),
             ),
           ),
-          Positioned(
+        ),
+        Positioned(
             bottom: -200,
             right: 0,
             left: 0,
@@ -36,23 +54,20 @@ class HomePage extends StatelessWidget {
                   width: 400,
                   decoration: BoxDecoration(
                     gradient: const LinearGradient(
-                    begin: Alignment.bottomRight,  
-                    end: Alignment.bottomLeft,
-                    colors: [
-                     
-                       Color.fromRGBO(218, 147, 188, 1),
-                       Color.fromRGBO(98, 174, 190, 1),
-                       Color.fromRGBO(130, 90, 160, 1),
-                    ]
-                    ),
+                        begin: Alignment.bottomRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Color.fromRGBO(218, 147, 188, 1),
+                          Color.fromRGBO(98, 174, 190, 1),
+                          Color.fromRGBO(130, 90, 160, 1),
+                        ]),
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(200),
                   ),
                 ),
               ),
-            )
-          ),
-          Positioned.fill(
+            )),
+        Positioned.fill(
             bottom: -60,
             child: Align(
               alignment: Alignment.center,
@@ -60,14 +75,11 @@ class HomePage extends StatelessWidget {
                 width: 350,
                 height: 600,
                 decoration: BoxDecoration(
-                  color: Color.fromRGBO(53, 61, 80, 0.8),
-                  borderRadius: BorderRadius.circular(15)
-                ),
+                    color: Color.fromRGBO(53, 61, 80, 0.8),
+                    borderRadius: BorderRadius.circular(15)),
               ),
-            )
-          )
-        ],
-      )
-    );
+            ))
+      ],
+    ));
   }
 }
